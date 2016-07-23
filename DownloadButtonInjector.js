b42bf160de116412db33ecbfbd0f5bebe46b8fae
Swastik Mishra <coolswastik60@gmail.com>
2016-07-24 02:38:37 +0530

    Update DownloadButtonInjector.js

--- DownloadButtonInjector.js
+++ DownloadButtonInjector.js
@@ -5,21 +5,23 @@ setInterval(checkNewElements, 100);
 
 function checkNewElements(){
     var InjectableElements = document.getElementsByClassName('_jjzlb');
-    var imgs = document.getElementsByClassName('_icyx7');
-    var aLink = '<a style="background-color: #003569;color: white;padding: 10px;border-radius: 4px;;top: 20px;left: 10px;text-align: center;z-index: 999999999;position: relative;border-bottom: 1px solid #ccc;">Download</a>';
+    if(oldInjectElements < InjectableElements.length)
+      injectElements();
+}
 
-    if(oldInjectElements < InjectableElements.length){
+function injectElements(){
+  var InjectableElements = document.getElementsByClassName('_jjzlb');
+  var imgs = document.getElementsByClassName('_icyx7');
+  var aLink = '<a style="background: linear-gradient(to top, #4B5475 0%, rgb(102, 94, 160) 100%);color: white;padding: 10px;padding-bottom: 5px;border-radius: 4px;padding-top: 7px;text-align: center; position: relative;float: right;border-bottom: 1px solid #ccc;">Download</a>';
+  var injectionSpots = document.getElementsByClassName('_iuf51 _oajsw');
 
-        var newElements = InjectableElements.length-oldInjectElements;
+      var newElements = InjectableElements.length-oldInjectElements;
 
-        for(var x = oldInjectElements; x < InjectableElements.length; x++){
-            InjectableElements[x].innerHTML += aLink;
-            InjectableElements[x].getElementsByTagName('a')[0].href = imgs[x].src;
-            console.log(InjectableElements[x].getElementsByTagName('a')[0].href);
-            InjectableElements[x].getElementsByTagName('a')[0].onclick = function() { alert(imgs[x].src); }
-        }
+      for(var x = oldInjectElements; x < InjectableElements.length; x++){
+          injectionSpots[x].innerHTML += aLink;
+          injectionSpots[x].getElementsByTagName('a')[0].href = imgs[x].src
+      }
 
-        oldInjectElements += newElements; 
-        console.log(oldInjectElements + " : " + InjectableElements.length);
-    }
+      oldInjectElements += newElements;
+      console.log(oldInjectElements + " : " + InjectableElements.length);
 }
